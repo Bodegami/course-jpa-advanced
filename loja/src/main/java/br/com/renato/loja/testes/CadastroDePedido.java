9c58e7a2340ec8b57e2807e36ce7ab6122d57dca
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2021-07-13 21:43:36 -0300

    Incrementando dados para base de testes

--- loja/src/main/java/br/com/renato/loja/testes/CadastroDePedido.java
+++ loja/src/main/java/br/com/renato/loja/testes/CadastroDePedido.java
@@ -28,15 +28,24 @@ public class CadastroDePedido {
 		ClienteDao clienteDao = new ClienteDao(em);
 		
 		Produto produto = produtoDao.buscarPorId(1l);
+		Produto produto2 = produtoDao.buscarPorId(2l);
+		Produto produto3 = produtoDao.buscarPorId(3l);
+		
 		Cliente cliente = clienteDao.buscarPorId(1l);
 		
 		em.getTransaction().begin();
 
 		Pedido pedido = new Pedido(cliente);
 		pedido.adicionarItem(new ItemPedido(10, pedido, produto));
+		pedido.adicionarItem(new ItemPedido(40, pedido, produto2));
+		
+		Pedido pedido2 = new Pedido(cliente);
+		pedido.adicionarItem(new ItemPedido(2, pedido, produto3));
+		
 		
 		PedidoDao pedidoDao = new PedidoDao(em);
 		pedidoDao.cadastrar(pedido);
+		pedidoDao.cadastrar(pedido2);
 		
 		em.getTransaction().commit();
 		
@@ -53,8 +62,14 @@ public class CadastroDePedido {
 	}
 	
 	private static void popularBancoDeDados() {
-		Categoria celulares = new Categoria("celulares");
+		Categoria celulares = new Categoria("CELULARES");
+		Categoria videogames = new Categoria("VIDEOGAMES");
+		Categoria informatica = new Categoria("INFORMATICA");
+		
 		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
+		Produto videogame = new Produto("PS5", "Playstation 5", new BigDecimal("4800"), videogames);
+		Produto macbook = new Produto("MacBook", "Macbook pro retina", new BigDecimal("6000"), informatica);
+		
 		Cliente cliente = new Cliente("Renato", "123456");
 
 		EntityManager em = JPAUtil.getEntityManager();
@@ -66,7 +81,13 @@ public class CadastroDePedido {
 		em.getTransaction().begin();
 		
 		categoriaDao.cadastrar(celulares);
+		categoriaDao.cadastrar(videogames);
+		categoriaDao.cadastrar(informatica);
+		
 		produtoDao.cadastrar(celular);
+		produtoDao.cadastrar(videogame);
+		produtoDao.cadastrar(macbook);
+		
 		clienteDao.cadastrar(cliente);
 		
 		em.getTransaction().commit();
