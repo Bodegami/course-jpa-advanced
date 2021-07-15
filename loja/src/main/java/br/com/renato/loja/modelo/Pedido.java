87ddcdcbbe53d0c42653391f9554b052970c8036
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2021-07-14 22:22:43 -0300

    add Teste PerformanceConsulta & entendendo melhor sobre fetch = LAZY

--- loja/src/main/java/br/com/renato/loja/modelo/Pedido.java
+++ loja/src/main/java/br/com/renato/loja/modelo/Pedido.java
@@ -8,6 +8,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -22,33 +23,40 @@ public class Pedido {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(name = "valor_total")
 	private BigDecimal valorTotal = BigDecimal.ZERO;
-	
-	private LocalDate data = LocalDate.now();
 
-	@ManyToOne
-	private Cliente cliente;
+	private LocalDate data = LocalDate.now();
 	
 	/*
-		Sempre que trabalhamos com um relacionamento
-		bidirecional, precisamos colocar o mappedBy
-		apontando para atributo em questao na outra 
-		classe do relacionamento.
-		
-		É uma boa pratica sempre inicializar
-		a lista para evitar mais if's no codigo.
-		
-		O cascade cria um efeito cascata nas operações 
-		realizadas em uma entidade.
-		
-		Utilizamos o cascade type ALL, para que quando
-		um pedido for salvo, alterado ou excluido do
-		banco, o mesmo se replique na classe ItemPedido,
-		dessa forma garantimos que a consistencia das
-		informações.
+		Todo relacionamento toOne, o padrao de carregamento
+		é EAGER, ou seja, ele faz o carregamento antecipado
+		mesmo que não seja a informacao que buscamos no 
+		metodo que chamou a classe.
+		A boa pratica diz que em todo carregamento toOne, 
+		coloque o fetch para LAZY, assim ganhamos em performance.
 	*/
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Cliente cliente;
+
+	/*
+	 * Sempre que trabalhamos com um relacionamento bidirecional, precisamos colocar
+	 * o mappedBy apontando para atributo em questao na outra classe do
+	 * relacionamento.
+	 * 
+	 * É uma boa pratica sempre inicializar a lista para evitar mais if's no codigo.
+	 * 
+	 * O cascade cria um efeito cascata nas operações realizadas em uma entidade.
+	 * 
+	 * Utilizamos o cascade type ALL, para que quando um pedido for salvo, alterado
+	 * ou excluido do banco, o mesmo se replique na classe ItemPedido, dessa forma
+	 * garantimos que a consistencia das informações.
+	 * 
+	 * Todo os relacionamentos toMany, por padrao seguem o carregamento LAZY, ou
+	 * seja, é o carregamento preguiço ou tardio.
+	 */
 	@OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
 	private List<ItemPedido> itens = new ArrayList<>();
 
@@ -91,14 +99,24 @@ public class Pedido {
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
-	
+
+	public List<ItemPedido> getItens() {
+		return itens;
+	}
+
 	/*
-		É considerado uma boa pratica criar um método utilitario que
-		tem como responsabilidade setar o item a lista de itensPedido e 
-		por ser um relacionamento bidirecional, fazer o mesmo do outro
-		lado como no exemplo abaixo.
-	*/
-	
+	 * É considerado uma boa pratica criar um método utilitario que tem como
+	 * responsabilidade setar o item a lista de itensPedido e por ser um
+	 * relacionamento bidirecional, fazer o mesmo do outro lado como no exemplo
+	 * abaixo.
+	 */
+
+	@Override
+	public String toString() {
+		return "Pedido [id=" + id + ", valorTotal=" + valorTotal + ", data=" + data + ", cliente=" + cliente
+				+ ", itens=" + itens + "]";
+	}
+
 	public void adicionarItem(ItemPedido item) {
 		item.setPedido(this);
 		this.itens.add(item);
---
+++ loja/src/main/java/br/com/renato/loja/testes/PerformanceConsultas.java
@@ -0,0 +1,95 @@
+package br.com.renato.loja.testes;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+
+import br.com.renato.loja.dao.CategoriaDao;
+import br.com.renato.loja.dao.ClienteDao;
+import br.com.renato.loja.dao.PedidoDao;
+import br.com.renato.loja.dao.ProdutoDao;
+import br.com.renato.loja.modelo.Categoria;
+import br.com.renato.loja.modelo.Cliente;
+import br.com.renato.loja.modelo.ItemPedido;
+import br.com.renato.loja.modelo.Pedido;
+import br.com.renato.loja.modelo.Produto;
+import br.com.renato.loja.util.JPAUtil;
+
+public class PerformanceConsultas {
+	
+	public static void main(String[] args) {
+		
+		popularBancoDeDadosComProdutos();
+		popularBancoDeDadosComPedidos();
+		EntityManager em = JPAUtil.getEntityManager();
+		Pedido pedido = em.find(Pedido.class, 1l);
+		System.out.println(pedido.getItens().size());
+		
+	}
+	
+	
+	private static void popularBancoDeDadosComProdutos() {
+		Categoria celulares = new Categoria("CELULARES");
+		Categoria videogames = new Categoria("VIDEOGAMES");
+		Categoria informatica = new Categoria("INFORMATICA");
+		
+		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
+		Produto videogame = new Produto("PS5", "Playstation 5", new BigDecimal("4800"), videogames);
+		Produto macbook = new Produto("MacBook", "Macbook pro retina", new BigDecimal("6000"), informatica);
+		
+		Cliente cliente = new Cliente("Renato", "123456");
+
+		EntityManager em = JPAUtil.getEntityManager();
+		
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		CategoriaDao categoriaDao = new CategoriaDao(em);
+		ClienteDao clienteDao = new ClienteDao(em);		
+
+		em.getTransaction().begin();
+		
+		categoriaDao.cadastrar(celulares);
+		categoriaDao.cadastrar(videogames);
+		categoriaDao.cadastrar(informatica);
+		
+		produtoDao.cadastrar(celular);
+		produtoDao.cadastrar(videogame);
+		produtoDao.cadastrar(macbook);
+		
+		clienteDao.cadastrar(cliente);
+		
+		em.getTransaction().commit();
+		em.close();
+	}
+
+	private static void popularBancoDeDadosComPedidos() {
+
+		EntityManager em = JPAUtil.getEntityManager();
+		
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		ClienteDao clienteDao = new ClienteDao(em);
+		
+		Produto produto = produtoDao.buscarPorId(1l);
+		Produto produto2 = produtoDao.buscarPorId(2l);
+		Produto produto3 = produtoDao.buscarPorId(3l);
+		
+		Cliente cliente = clienteDao.buscarPorId(1l);
+		
+		em.getTransaction().begin();
+
+		Pedido pedido = new Pedido(cliente);
+		pedido.adicionarItem(new ItemPedido(10, pedido, produto));
+		pedido.adicionarItem(new ItemPedido(40, pedido, produto2));
+		
+		Pedido pedido2 = new Pedido(cliente);
+		pedido.adicionarItem(new ItemPedido(2, pedido, produto3));
+		
+		
+		PedidoDao pedidoDao = new PedidoDao(em);
+		pedidoDao.cadastrar(pedido);
+		pedidoDao.cadastrar(pedido2);
+		
+		em.getTransaction().commit();
+		
+	}
+	
+}
