5b2ce73bcc5efaf752bd66e474a7d3dce1e60c26
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2021-07-15 15:57:03 -0300

    add consulta com criteria

---
+++ loja/src/main/java/br/com/renato/loja/testes/TesteCriteria.java
@@ -0,0 +1,62 @@
+package br.com.renato.loja.testes;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import br.com.renato.loja.dao.CategoriaDao;
+import br.com.renato.loja.dao.ClienteDao;
+import br.com.renato.loja.dao.ProdutoDao;
+import br.com.renato.loja.modelo.Categoria;
+import br.com.renato.loja.modelo.Cliente;
+import br.com.renato.loja.modelo.Produto;
+import br.com.renato.loja.util.JPAUtil;
+
+public class TesteCriteria {
+
+	public static void main(String[] args) {
+		popularBancoDeDados();
+		
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		List<Produto> produtos = produtoDao.buscarPorParametrosComCriteria("PS5", null, LocalDate.now());
+		System.out.println(produtos);
+	}
+	
+	
+	
+	private static void popularBancoDeDados() {
+		Categoria celulares = new Categoria("CELULARES");
+		Categoria videogames = new Categoria("VIDEOGAMES");
+		Categoria informatica = new Categoria("INFORMATICA");
+		
+		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
+		Produto videogame = new Produto("PS5", "Playstation 5", new BigDecimal("4800"), videogames);
+		Produto macbook = new Produto("MacBook", "Macbook pro retina", new BigDecimal("6000"), informatica);
+		
+		Cliente cliente = new Cliente("Renato", "123456");
+
+		EntityManager em = JPAUtil.getEntityManager();
+		
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		CategoriaDao categoriaDao = new CategoriaDao(em);
+		ClienteDao clienteDao = new ClienteDao(em);		
+
+		em.getTransaction().begin();
+		
+		categoriaDao.cadastrar(celulares);
+		categoriaDao.cadastrar(videogames);
+		categoriaDao.cadastrar(informatica);
+		
+		produtoDao.cadastrar(celular);
+		produtoDao.cadastrar(videogame);
+		produtoDao.cadastrar(macbook);
+		
+		clienteDao.cadastrar(cliente);
+		
+		em.getTransaction().commit();
+		em.close();
+	}
+}
